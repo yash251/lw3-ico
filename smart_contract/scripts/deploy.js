ab18dc7d8eb211bdc5f087f463c6879b70467e59
Yash Jagtap <jagtapyash2512@gmail.com>
2022-09-05 14:54:38 +0530

    typo

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,9 +1,9 @@
 const { ethers } = require("hardhat");
 require("dotenv").config({ path: ".env" });
-const { CRYPTODEVS_NFT_CONTRACT_ADDRESS } = require("../constants");
+const { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS } = require("../constants");
 
 async function main() {
-  const cryptoDevsNFTContract = CRYPTODEVS_NFT_CONTRACT_ADDRESS;
+  const cryptoDevsNFTContract = CRYPTO_DEVS_NFT_CONTRACT_ADDRESS;
 
   const cryptoDevsTokenContract = await ethers.getContractFactory("CryptoDevToken");
   const deployedCryptoDevsTokenContract = await cryptoDevsTokenContract.deploy(cryptoDevsNFTContract);
